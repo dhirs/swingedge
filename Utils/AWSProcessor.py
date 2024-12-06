9d4b7d55881a42a41d379668cf38a77bf7669d91
AnurupaK <anurupakarmakar.dgp18@gmail.com>
2024-12-06 14:42:32 +0530

    modification in awsprocessor

--- Utils/AWSProcessor.py
+++ Utils/AWSProcessor.py
@@ -1,4 +1,4 @@
-// AWS credentials
+# AWS credentials
 
 def send_msg_to_aws(msg):
     pass
@@ -7,10 +7,10 @@ import boto3
 ## Get the service resource
 sqs = boto3.resource('sqs')
 def send_message_sqs(q_url, msg_body, message_attributes=None):
-queue_url = 'https://sqs.ap-south-1.amazonaws.com/863372932275/CandleData'
-resp = sqs.send_message(
-QueueUrl=queue_url,
-MessageBody=(
-msg_body
-)
-)
+    queue_url = 'https://sqs.ap-south-1.amazonaws.com/863372932275/CandleData'
+    resp = sqs.send_message(
+    QueueUrl=queue_url,
+    MessageBody=(
+    msg_body
+    )
+    )
